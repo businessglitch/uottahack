0ed6988044ce5fc3a13c2677268b5c11fb702ad7
Yunwei Li <yli@pythian.com>
2018-02-17 17:11:23 -0500

    added database and mock data

--- journey/src/App.js
+++ journey/src/App.js
@@ -4,6 +4,22 @@ import './App.css';
 import Header from './components/header.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {venues: null};
+  }
+
+  async callApi() {
+    var response = await fetch('/api/venues');
+    var body = await response.json();
+    console.log(body);
+  }
+
+  componentDidMount() {
+    this.callApi().then(function(){}).catch(function(){});
+  }
+
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var express = require('express');
+const admin = require('firebase-admin');
+var config = require('./config/database');
 
 var app = express();
-
 var port = 5000;
+
+var serviceAccount = require('./config/serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+
+var venueList = [];
 
 app.listen(port, function() {
     console.log('Journey server listening on port 5000');
+    db.collection('venues').get()
+        .then(function(venues) {
+            venues.forEach(function(venue) {
+                venueList.push(venue.data());
+            });
+        })
+        .catch(function(err) {
+
+        });
 });
 
-app.get('/', function(req, res) {
-    res.send('Invalid point');
+app.get('/api/venues', function(req, res) {
+    res.send(venueList);
 })
