1878e1b2c0fd13d8483301e3c0b0144be677bcc1
Fahad Hayat <fahadhayat@Fahads-MacBook-Pro.local>
2018-02-17 22:24:15 -0500

    Added changes to the JourneyDetail component

--- journey/src/components/JourneyDetail.js
+++ journey/src/components/JourneyDetail.js
@@ -4,9 +4,31 @@ import './JourneyDetail.css';
 class JourneyDetail extends Component {
   render() {
     return (
-      <div className="Header-title">
-        <h1>JourneyDetail</h1>
-      </div>
+    <div className="">
+        <div className="ui vertical steps">
+  			<div className="completed step">
+    			<i className="truck icon"></i>
+    			<div className="content">
+      				<div className="title">Shipping</div>
+      				<div className="description">Choose your shipping options</div>
+    			</div>
+  			</div>
+  			<div className="completed step">
+    			<i className="credit card icon"></i>
+   				<div className="content">
+      				<div className="title">Billing</div>
+      				<div className="description">Enter billing information</div>
+    			</div>
+  			</div>
+  			<div className="active step">
+    			<i className="info icon"></i>
+    			<div className="content">
+				   	<div className="title">Confirm Order</div>
+				    <div className="description">Verify order details</div>
+    			</div>
+  			</div>
+		</div>
+    </div>
     );
   }
 }
