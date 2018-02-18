76deccfd5428b720882090f8ff52acb5dde0cc42
TunaButter <yunwei.li@carleton.ca>
2018-02-18 05:14:34 -0500

    stash

--- journey/src/components/JourneyDetail.js
+++ journey/src/components/JourneyDetail.js
@@ -6,11 +6,15 @@ import {
 } from 'react-router-dom';
 
 class JourneyDetail extends Component {
-	 constructor(props) {
-	    super(props);
+	constructor(props) {
+		super(props);
 
 	    this.state = { journey: Journeys[this.props.match.params.id]};
-	  }
+	}
+
+	getCoordinate(lat, lon) {
+		console.log(lat + ' ' + lon);
+	}
 
 	componentDidMount() {
 		this.setState({
@@ -23,10 +27,10 @@ class JourneyDetail extends Component {
 		    	<div>
 		        	<div className="ui vertical steps">
 				        {this.state.journey.events.map((item,i) => (
-							<div key={i} className="step">
+							<div key={i} className="step" onClick={this.getCoordinate(item.venue.lat, item.venue.lon).bind(this)}>
 				    			<div className="content">
 				      				<div className="title">{item.name}</div>
-				      				<div className="description"> {"Location:" + item.venue.name + item.venue.address_1}</div>
+				      				<div className="description">{"Location:" + item.venue.name + item.venue.address_1}</div>
 									<div className="description">{"Time:" + item.local_time}</div>
 				    			</div>
 				  			</div>
